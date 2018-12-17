6f048fc1665b8df7ddbe1d1e620d364e3af63472
MimiMagusin <mimi@codaisseur.com>
2018-12-17 11:04:21 +0100

    Set up starter code for running the program

--- 02-OOP-with-C#/stacks/StackService/Program.cs
+++ 02-OOP-with-C#/stacks/StackService/Program.cs
@@ -1,7 +1,37 @@
+using System;
 public static class Program
 {
-  public static void Main(string[] args)
+  static void Main()
   {
+    //Print initial messages when the program stars
+    Console.WriteLine("Welcome to the program.");
+    Console.WriteLine("Please input some numbers followed by <enter>. When you are done, press r to print the data structure, or input q to quit.");
 
+    while (true)
+    {
+      //Read inputs from the command line
+      //Waits until a line is being read from the console
+      string inputFromConsole = Console.ReadLine();
+
+      if (inputFromConsole == "r")
+      {
+        //Print 
+        Console.WriteLine("Print");
+        //Reset 
+      }
+
+      if (inputFromConsole == "q")
+      {
+        Console.WriteLine("Exiting... Bye!!");
+        return;
+      }
+
+      //If the input can be parsed as an Integer
+      if (int.TryParse(inputFromConsole, out int inputNumber))
+      {
+        //Add inputNumber to the data structure
+        Console.WriteLine($"Added {inputNumber}.");
+      }
+    }
   }
 }
